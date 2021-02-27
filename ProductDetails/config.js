d1a98e4c7d7f36852fcd5359e2a88fe42f13e505
Momo Sun <mosun1113@gmail.com>
2021-02-27 14:43:57 -0800

    delete bundle and config

--- ProductDetails/config.js
+++
@@ -1,6 +0,0 @@
-const api = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
-const TOKEN = 'd679bb529f34962ad8c17a7c22fc5d2738dd166c';
-
-module.exports = {
-  api, TOKEN,
-};
--- ProductDetails/src/components/App.js
+++ ProductDetails/src/components/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
-
 class App extends React.Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <div>hi</div>
+        )
+    }
 
 }
+export default App;
