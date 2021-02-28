e5492d9e992ef24a6073418d2fcac81ea1b6a0bd
Anna Zhu <zeehaychoo@gmail.com>
2021-02-27 17:08:34 -0800

    Adding API request handling for Atelier

--- QandA/server.js
+++ QandA/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const api = require('./config.js');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/products/:id', (req, res) => {
+  axios.get(`${api.api}/products`, {
+    headers: {
+      Authorization: api.TOKEN,
+    },
+  })
+    .then((data) => {
+      res.send(data.data);
+    })
+    .catch((err) => console.log(err));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening in on port ${PORT}`);
