3fd15fae9ac15eabd41506f8311fef848274e359
Anna Zhu <zeehaychoo@gmail.com>
2021-02-27 16:52:30 -0800

    Integrated ESLint

--- QandA/src/components/QuestionsList.jsx
+++ QandA/src/components/QuestionsList.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const QuestionsList = (props) => {
-  return (
+const QuestionsList = () => (
+  <div>
+    Test
     <div>
-      Test
-      <div>Question 1:
-        <div>Answers:</div>
-      </div>
-      <br />
-      <div>Question 2
-        <div>Answers:</div>
-      </div>
+      Question 1:
+      <div>Answers:</div>
     </div>
-  );
-}
+    <br />
+    <div>
+      Question 2
+      <div>Answers:</div>
+    </div>
+  </div>
+);
 
 export default QuestionsList;
