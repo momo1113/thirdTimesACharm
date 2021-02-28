489770cd9849fe23dda39e8cf36c8a6f4ce30486
Momo Sun <mosun1113@gmail.com>
2021-02-27 21:04:20 -0800

    basic components struture

---
+++ ProductDetails/src/components/ProductInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductInfo = () => {
+    return (
+        <div> Here is the bottom product info section</div>
+    )
+}
+
+export default ProductInfo;
--- ProductDetails/src/index.js
+++ ProductDetails/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/extensions
-import Products from './components/Products.js';
+import Products from './components/Products.jsx';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<Products />, document.getElementById('app'));
