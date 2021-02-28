0c1b75f58eb09f3fc87fb2bd91bca32ce85ac84c
Momo Sun <mosun1113@gmail.com>
2021-02-28 01:44:03 -0800

    test endpoint render thumbnails and url images

--- ProductDetails/src/components/LeftSection/ImageCarousel.jsx
+++ ProductDetails/src/components/LeftSection/ImageCarousel.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const ImageCarousel = ({styles}) => (
-  <div>
-    Here is the image carousel
-  </div>
-);
+const ImageCarousel = ({ styles }) => {
+  if (styles.length !== 0) {
+    console.log(styles);
+  }
+  return (
+    <div>
+      Here is the image carousel
+    </div>
+  );
+};
 
 export default ImageCarousel;
