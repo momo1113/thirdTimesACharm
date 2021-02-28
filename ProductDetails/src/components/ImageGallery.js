3c35415faa5cbf31e686e6109f1fad9ac11c69b6
Momo Sun <mosun1113@gmail.com>
2021-02-27 19:27:21 -0800

    basic components structure

---
+++ ProductDetails/src/components/ImageGallery.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ImageGallery extends React.Component {
+
+    constructor() {
+
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+
+    }
+}
+
+
+export default ImageGallery;
+
--- ProductDetails/src/index.js
+++ ProductDetails/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/extensions
-import App from './components/App.js';
+import Products from './components/Products.js';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Products />, document.getElementById('app'));
