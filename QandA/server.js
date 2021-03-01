15ad5662f475db85bdb14546a796abecebfa677a
Anna Zhu <zeehaychoo@gmail.com>
2021-02-28 22:45:16 -0800

    Implemented server caching of questions by id and product display

--- QandA/server.js
+++ QandA/server.js
@@ -10,16 +10,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 let products = [];
+const questions = {};
 
-app.get('/products/', (req, res) => {
+app.get('/products', (req, res) => {
   if (products.length !== 0) {
     res.send(products);
+  } else {
+    res.send();
   }
-  res.send();
+});
+
+app.get('/questions/:id', (req, res) => {
+  const productId = req.params.id;
+  if (questions[productId] === undefined) {
+    axios.get(`${api.api}/qa/questions?product_id=${productId}`, {
+      headers: {
+        Authorization: api.TOKEN,
+      },
+    })
+      .then((response) => {
+        questions[productId] = response.data.results;
+        res.send(questions);
+      })
+      .catch((err) => res.send(JSON.stringify(err)));
+  } else {
+    res.send(questions[productId]);
+  }
+});
+
+app.get('/q', (req, res) => {
+  res.send(questions);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening in on port ${PORT}`);
+  // console.log(`Server listening in on port ${PORT}`);
   axios.get(`${api.api}/products`, {
     headers: {
       Authorization: api.TOKEN,
--- QandA/src/QA.jsx
+++ QandA/src/QA.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import QuestionsList from './components/QuestionsList.jsx';
 import SearchQuestions from './components/SearchQuestions.jsx';
 
-const QA = (props) => {
+const QA = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/products')
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, []);
+  console.log(products);
   return (
     <div>
+      <div>
+        <h1>Products</h1>
+        {products.map((product) => <p key={product.id}>{JSON.stringify(product)}</p>)}
+      </div>
       <h1>Questions & Answers</h1>
       <SearchQuestions />
       <br />
       <QuestionsList />
     </div>
   );
-}
+};
 
 ReactDOM.render(<QA />, document.getElementById('app'));
