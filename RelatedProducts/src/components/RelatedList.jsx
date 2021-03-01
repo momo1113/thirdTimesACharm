70f7fbecb1cb458655b5469bfcafa20f75efad66
jincheng411 <jincheng411@gmail.com>
2021-03-01 01:07:13 -0500

    add button scroll left and right for related products list

--- RelatedProducts/src/components/RelatedList.jsx
+++ RelatedProducts/src/components/RelatedList.jsx
@@ -11,12 +11,31 @@ class RelatedList extends React.Component {
     console.log('handlecompare')
   }
 
+  handleScroll(e) {
+    e.preventDefault();
+    const list = document.getElementById('relatedList');
+    console.log(e.target.id)
+    switch(e.target.id) {
+      case 'left':
+        list.scrollLeft -= 80;
+        break;
+
+        case 'right':
+          list.scrollLeft += 80;
+          break;
+    }
+  }
+
   render() {
     return (
       <div className="container">
-        {this.props.relatedList.map((item) => {
-          return <Card handleClick={this.handleCompare} key={item} id={item}/>
-        })}
+        <button onClick={this.handleScroll} id="left">&lt;&lt;</button>
+        <div className="list" id="relatedList">
+          {this.props.relatedList.map((item) => {
+            return <Card handleClick={this.handleCompare} key={item} id={item} />
+          })}
+        </div>
+        <button onClick={this.handleScroll} id="right">&gt; &gt; </button>
       </div>
     )
   }
