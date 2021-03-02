6199eb151239d4783239434a8393af8c9342e259
Momo Sun <mosun1113@gmail.com>
2021-03-02 11:11:49 -0800

    change rending rating as decimal; add logic if there is no reviews, review stars will not render on the DOM at all

--- ProductDetails/src/components/RightSection/TopSection.jsx
+++ ProductDetails/src/components/RightSection/TopSection.jsx
@@ -2,7 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import StarRatings from 'react-star-ratings';
-import { Reviews, Category, Name, Price } from '../../elements/RightSection/TopSection.element.jsx';
+import {
+  Reviews, Category, Name, Price,
+} from '../../elements/RightSection/TopSection.element.jsx';
 
 // eslint-disable-next-line react/prop-types
 const TopSection = ({ product, styles }) => {
@@ -20,19 +22,27 @@ const TopSection = ({ product, styles }) => {
 
   return (
     <div className="top_section">
-      <Reviews>
-        <StarRatings
-          rating={Number(rating)}
-          starRatedColor="RGB(253, 204, 13)"
-          numberOfStars={5}
-          starDimension={18}
-          starSpacing={1}
-          name="rating"
-        />
-      </Reviews>
+      {
+        Number(rating) > 0 && (
+        <Reviews>
+          <StarRatings
+            rating={Number(rating)}
+            starRatedColor="RGB(253, 204, 13)"
+            numberOfStars={5}
+            starDimension={18}
+            starSpacing={1}
+            name="rating"
+          />
+        </Reviews>
+        )
+      }
+
       <Category>{product.category}</Category>
       <Name>{product.name}</Name>
-      <Price>${product.default_price}</Price>
+      <Price>
+        $
+        {product.default_price}
+      </Price>
     </div>
   );
 };
