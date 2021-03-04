7b57eaffcd3c77246ed30be9ff45b220a2cc7851
Anna Zhu <zeehaychoo@gmail.com>
2021-03-03 19:58:44 -0800

    Added limiting answer list view to two at a time, with expandable functionalilty

--- QandA/src/components/AnswersList.jsx
+++ QandA/src/components/AnswersList.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Answer from './Answer.jsx';
 
 const AnswersList = ({ answersQ }) => {
+  const [moreAns, setMoreAns] = useState(2);
   return (
     <div>
-      {answersQ.length > 0
-        ? answersQ.map((answer) => (
-          // {/* {console.log(answerId)} */ }
-          <Answer key={answer.answer_id} answer={answer} />
-        ))
-        : <button type="button">Add an answer</button>
-      }
+      {answersQ.map((answer, index) => (index < moreAns
+        // {/* {console.log(answerId)} */ }
+        && <Answer key={answer.answer_id} answer={answer} />
+      ))}
+      <div>
+        {answersQ.length - moreAns > 0
+        && <button type="button" onClick={() => setMoreAns(moreAns + 2)}>LOAD MORE ANSWERS</button>}
+        <br />
+        <a href="jflda">Add Answer</a>
+      </div>
     </div>
   );
 };
--- QandA/src/components/QuestionsList.jsx
+++ QandA/src/components/QuestionsList.jsx
@@ -2,26 +2,25 @@ import React, { useState } from 'react';
 import Question from './Question.jsx';
 
 const QuestionsList = ({ questions, questionsId, answers }) => {
-  const [moreQ, setMoreQ] = useState(2);
+  const [moreQ, setMoreQ] = useState(4);
 
   return (
     <div>
       {/* {console.log(questions[0].question_id)} */}
-      {console.log(questions.length - moreQ, questions.length, moreQ)}
-      {moreQ
-        && questions.map((question, index) => (index < moreQ
-            && (
-            <Question
-              key={question.question_id}
-              question={question}
-              answersQ={answers[question.question_id].results}
-            />
-            )
-        ))}
+      {/* {console.log(questions.length - moreQ, questions.length, moreQ)} */}
+      {questions.map((question, index) => (index < moreQ
+        && (
+          <Question
+            key={question.question_id}
+            question={question}
+            answersQ={answers[question.question_id].results}
+          />
+        )
+      ))}
       <div>
         {questions.length - moreQ > 0
-          && <button type="button" onClick={() => setMoreQ(moreQ + 2)}>More Answered Questions</button>}
-        <button type="button">Add a question</button>
+          && <button type="button" onClick={() => setMoreQ(questions.length)}>MORE ANSWERED QUESTIONS</button>}
+        <button type="button">ADD A QUESTION</button>
       </div>
     </div>
   );
