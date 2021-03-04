355fe3b4dbfa8c0d689aff9361926dbcde9e8f94
jincheng411 <jincheng411@gmail.com>
2021-03-04 13:31:30 -0500

    create multiple css files

--- RelatedProducts/src/components/OutfitList.jsx
+++ RelatedProducts/src/components/OutfitList.jsx
@@ -26,25 +26,29 @@ class OutfitList extends React.Component {
   render() {
     const { currentImg, outfitList, handleScroll, currentProduct } = this.props;
     return (
-      <div className="container">
-        <button onClick={this.handleScroll} id="left">&lt;&lt;</button>
-        <div className="list" id="outfitList">
+      <div>
 
-          <div className="card" onClick={this.addToList}>
-            <div className="addList">
-              <div className="frame">
-                <img src={currentImg} alt="image" />
+
+        <div className="container">
+          <div className="scroll-btn" onClick={this.handleScroll} id="left">&lt;</div>
+          <div className="list" id="outfitList">
+
+            <div className="card" onClick={this.addToList}>
+              <div className="addList">
+                <div className="frame">
+                  <img src={currentImg} alt="image" />
+                </div>
+                <div>Add To Outfit List</div>
+                <div id="addSign">+</div>
               </div>
-              <div>Add To Outfit List</div>
-              <div id="addSign">+</div>
             </div>
-          </div>
 
-          {outfitList.map(
-            (item) => <Card handleClick={this.removeFromList} key={item} id={item} list="outfitList" currentProduct={currentProduct} />,
-          )}
+            {outfitList.map(
+              (item) => <Card handleClick={this.removeFromList} key={item} id={item} list="outfitList" currentProduct={currentProduct} />,
+            )}
+          </div>
+          <div onClick={this.handleScroll} id="right">&gt; </div>
         </div>
-        <button onClick={this.handleScroll} id="right">&gt; &gt; </button>
       </div>
     );
   }
