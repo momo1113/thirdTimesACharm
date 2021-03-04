f6a5721877d2498e62d9c817291f8df7c52ca7eb
gusfel <agustinjfeliciano@gmail.com>
2021-03-04 11:25:00 -0800

    made helpful number increase on click

--- Reviews/src/Components/ReviewList/ReviewHelpful.jsx
+++ Reviews/src/Components/ReviewList/ReviewHelpful.jsx
@@ -1,26 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const ReviewHelpful = ({ review,  markAsHelpful, reportReview}) => {
+const ReviewHelpful = ({ review, markAsHelpful, reportReview }) => {
   const [hClicked, setHClicked] = useState(false);
   const [rClicked, setRClicked] = useState(false);
-  let total = review.helpfulness;
-  const increase = () => { total++ }
+  const total = review.helpfulness;
+  const nextTotal = total + 1;
   return (
     <div>
       <p>
         Helpful? &nbsp;
         {!hClicked
-          ? <span className="link" onClick={() => { markAsHelpful(review.review_id); setHClicked(true); increase();}}>Yes</span>
-          : <span class="link" >Yes</span>}
-        &nbsp;
-        (
-        {total}
-        )&nbsp;&nbsp;|
+          ? (
+            <>
+              <span className="link" onClick={() => { markAsHelpful(review.review_id); setHClicked(true); }}>Yes</span>
+          &nbsp;
+              {total}
+            </>
+          )
+          : (
+            <>
+              <span className="link">Yes</span>
+          &nbsp;
+              {nextTotal}
+            </>
+          )}
+        &nbsp;&nbsp;|
         &nbsp;&nbsp;
         {!rClicked
-        ? <span className="link" onClick={() => { reportReview(review.review_id); setRClicked(true); }}>Report</span>
-        : <span class="link" >Report</span>}
+          ? <span className="link" onClick={() => { reportReview(review.review_id); setRClicked(true); }}>Report</span>
+          : <span className="link">Report</span>}
 
       </p>
     </div>
