87d077cc0054467629682a8fd6439a23e9d31920
Anna Zhu <zeehaychoo@gmail.com>
2021-03-04 17:09:59 -0800

    Made UTC date more readable

--- QandA/src/components/Answer.jsx
+++ QandA/src/components/Answer.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import Photos from './Photos.jsx';
 
-const Answer = ({ answer }) => (
-  <div>
-    {/* {console.log(JSON.stringify(answer))} */}
-    <p>{answer.body}</p>
-    {answer.photos.length > 0
-    && <Photos photos={answer.photos} />}
-    <p>
-      by {answer.answerer_name}, {answer.date}
-    </p>
-    <p>
-      Helpful? Yes({answer.helpfulness}) | Report
-    </p>
-  </div>
-);
+const Answer = ({ answer }) => {
+  const readableDate = new Date(answer.date);
+  const month = readableDate.getUTCMonth();
+  // difference between .getUTCDate and .getDate?
+  const day = readableDate.getDate();
+  const year = readableDate.getUTCFullYear();
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  return (
+    <div>
+      <p>{answer.body}</p>
+      {answer.photos.length > 0
+      && <Photos photos={answer.photos} />}
+      <p>
+        by
+        {' '}
+        {answer.answerer_name}
+        ,
+        {' '}
+        {months[month]}
+        {' '}
+        {day}
+        ,
+        {' '}
+        {year}
+      </p>
+      <p>
+        Helpful? Yes(
+        {answer.helpfulness}
+        ) | Report
+      </p>
+    </div>
+  );
+};
 
 export default Answer;
