b23ec6532cd9de934c3ccf2c0697cac1bd42b6d9
Anna Zhu <zeehaychoo@gmail.com>
2021-03-05 23:39:19 -0800

    Sorted answers list to prioritize Seller answers

--- QandA/src/components/AnswersList.jsx
+++ QandA/src/components/AnswersList.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import Answer from './Answer.jsx';
 
 const AnswersList = ({ answersQ, search, foundInQ }) => {
-  const [filteredAns, setFilteredAns] = useState(answersQ);
+  // const [filteredAns, setFilteredAns] = useState(answersQ);
   const [moreAns, setMoreAns] = useState(2);
+  const [sortedAns, setSortedAns] = useState(answersQ);
   // const [filtered, setFiltered] = useState(answersQ.length);
 
   const filterAns = answersQ.filter((answer) => {
@@ -15,20 +16,28 @@ const AnswersList = ({ answersQ, search, foundInQ }) => {
     return false;
   });
 
-  if (JSON.stringify(filteredAns) !== JSON.stringify(filterAns)) {
-    setFilteredAns(filterAns);
+  const sorted = [];
+  for (let i = filterAns.length - 1; i >= 0; i -= 1) {
+    const currentAns = filterAns[i];
+    if (currentAns.answerer_name.toLowerCase() === 'seller') {
+      sorted.unshift(currentAns);
+    } else {
+      sorted.push(currentAns);
+    }
+  }
+
+  if (JSON.stringify(sortedAns) !== JSON.stringify(sorted)) {
+    setSortedAns(sorted);
     setMoreAns(2);
   }
 
   return (
     <div>
-      {filteredAns.map((answer, index) => (index < moreAns
+      {sortedAns.map((answer, index) => (index < moreAns
         && <Answer key={answer.answer_id} answer={answer} />))}
       <>
-        {filteredAns.length - moreAns > 0
+        {sortedAns.length - moreAns > 0
           && <button type="button" onClick={() => setMoreAns(moreAns + 2)}>LOAD MORE ANSWERS</button>}
-        {/* <br /> */}
-        {/* <a href="jflda">Add Answer</a> */}
       </>
     </div>
   );
