38d2a8ef81b01c5bc63b82f76412a542977d4fb9
Momo Sun <mosun1113@gmail.com>
2021-03-06 15:43:30 -0800

    move file back to  the right folder

--- ProductDetails/src/components/RightSection/BottomSection.jsx
+++ ProductDetails/src/components/RightSection/BottomSection.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import StyleThumbnails from '../src/components/RightSection/StyleThumbnails.jsx';
 import QuanityAndSize from '../src/components/RightSection/QuanityAndSize.jsx';
